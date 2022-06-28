a674aaaacc153d2a12565b2edb4724b7e234fa74
gyojincompany <luckyred@gmail.com>
2022-06-28 16:02:00 +0900

    react git test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+
+import { Component, Fragment } from 'react';
+import Navi1 from './Navi1';
+import Content from './content/Content';
 import './App.css';
 
-function App() {
+// class Navi1 extends Component {
+//   render(){
+//     return(
+//       <nav>
+//         <h2>리액트 기본문법</h2>
+//         <h2>리액트 컴포넌트 만들기</h2>
+//         <h2>리액트 JSX 배우기</h2>
+//       </nav>
+//     );
+//   }
+// }
+
+class Navi2 extends Component {
+  render(){
+    return(
+      <nav>
+        <ul>
+          <li><a href='http://reactjs.org'><h2>리액트 props</h2></a></li>
+          <li><h2>리액트 state</h2></li>
+          <li><h2>리액트 create</h2></li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+// class Content extends Component {
+//   render(){
+//     return(
+//       <article>
+//         <h2>React</h2>
+//         React! 최고의 프론트 도구
+//       </article>
+//     );
+//   }
+// }
+
+class App extends Component {
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+    <header className='App'>
+      react를 배워 봅시다!!
+      <Content></Content>
+      <Navi1></Navi1>
+      <Navi2></Navi2>
+    </header>
+    <Content></Content>
+    </Fragment>
   );
+}
 }
 
 export default App;
